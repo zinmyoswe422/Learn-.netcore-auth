9413f3ef674c6a2831f35820863075fcd8415de6
zinmyoswe422 <zinmyoswe@cityparking.app>
2024-10-30 15:54:30 +0700

    Add project files.

---
+++ Areas/Identity/Data/DBContextSample.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Zinauth.Areas.Identity.Data;
+
+namespace Zinauth.Areas.Identity.Data;
+
+public class DBContextSample : IdentityDbContext<SampleUser>
+{
+    public DBContextSample(DbContextOptions<DBContextSample> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntitiyConfiguration());
+    }
+}
+
+public class ApplicationUserEntitiyConfiguration : IEntityTypeConfiguration<SampleUser>
+{
+    public void Configure(EntityTypeBuilder<SampleUser> builder)
+    {
+        builder.Property(x => x.FirstName).HasMaxLength(100);
+        builder.Property(x => x.LastName).HasMaxLength(100);
+
+    }
+}
